f59e49cc336c7b8356b6a183a1f2a46a3c787673
KotenkoNata <kotenkonata8@gmail.com>
2022-01-10 18:49:44 +0200

    feat: add styles for tablet, images, create burger-menu

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+let menuBtn = document.querySelector('.header-burger');
+let menu = document.querySelector('.burger-menu');
+
+menuBtn.addEventListener('click', function(){
+  menuBtn.classList.toggle('active');
+  menu.classList.toggle('active');
+})
+
+
+
 console.log(`Score: 100 / 100;\n 1. Вёрстка валидная +10/+10;\n 2. Вёрстка семантическая +20/20;\n 3. Вёрстка соответствует макету +48/48; \n 4. Требования к css + 12/12; \n 5. Интерактивность, реализуемая через css +20/20; \n`)
