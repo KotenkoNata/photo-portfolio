a60261811247caa950a2d70174170b381587bccd
KotenkoNata <kotenkonata8@gmail.com>
2022-01-12 16:50:15 +0200

    fix: styles for phone

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-let menuBtn = document.querySelector('.header-burger');
-let menu = document.querySelector('.burger-menu');
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector(".header-burger"),
+    menu: document.querySelector(".burger-menu"),
+    menuItems: document.querySelectorAll(".burger-menu li"),
+    body: document.querySelector('body'),
+  };
 
-menuBtn.addEventListener('click', function(){
-  menuBtn.classList.toggle('active');
-  menu.classList.toggle('active');
-})
+  function toggleModal() {
+    refs.openModalBtn.classList.toggle("active");
+    refs.menu.classList.toggle("active");
+    refs.body.classList.toggle('lock');
+  }
 
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.menuItems.forEach(el => {
+    el.addEventListener('click', toggleModal)
+  })
 
+})();
 
 console.log(`Score: 100 / 100;\n 1. Вёрстка валидная +10/+10;\n 2. Вёрстка семантическая +20/20;\n 3. Вёрстка соответствует макету +48/48; \n 4. Требования к css + 12/12; \n 5. Интерактивность, реализуемая через css +20/20; \n`)
