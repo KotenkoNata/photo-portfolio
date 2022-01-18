a6ff2fe50f45694a3261ae4148f22cb7e4f12669
KotenkoNata <kotenkonata8@gmail.com>
2022-01-18 21:24:39 +0200

    feat: add first part of task - changing image

--- index.js
+++ index.js
@@ -19,4 +19,22 @@
 
 })();
 
+//Смена изображений в секции Portfolio
+
+const portfolioBtns = document.querySelector('.portfolio-button-list');
+const portfolioImages = document.querySelectorAll('.portfolio-image');
+
+function changeImage(event) {
+  if(event.target.classList.contains('portfolio-button')) {
+    let season = event.target.dataset.season;
+    portfolioImages.forEach((img, index, ) => img.src = `assets/img/${season}/${index + 1}.jpg`)
+
+  }
+}
+
+portfolioBtns.addEventListener('click',changeImage);
+
+
+
+
 console.log(`Score: 75 / 75;\n 1. Вёрстка соответствует макету. Ширина экрана 768px +48/48;\n 2. Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки +15/15;\n 3. На ширине экрана 768рх и меньше реализовано адаптивное меню +22/22;`)
