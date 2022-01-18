744c453e392faaa37351cafa93b34c2a2f5d41b6
KotenkoNata <kotenkonata8@gmail.com>
2022-01-18 12:09:23 +0200

    feat: add burger-menu html and styles

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log(`Score: 100 / 100;\n 1. Вёрстка валидная +10/+10;\n 2. Вёрстка семантическая +20/20;\n 3. Вёрстка соответствует макету +48/48; \n 4. Требования к css + 12/12; \n 5. Интерактивность, реализуемая через css +20/20; \n`)
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector(".header-burger"),
+    menu: document.querySelector(".burger-menu"),
+    menuItems: document.querySelectorAll(".burger-menu li"),
+    body: document.querySelector('body'),
+  };
+
+  function toggleModal() {
+    refs.openModalBtn.classList.toggle("active");
+    refs.menu.classList.toggle("active");
+    refs.body.classList.toggle('lock');
+  }
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.menuItems.forEach(el => {
+    el.addEventListener('click', toggleModal)
+  })
+
+})();
+
+console.log(`Score: 75 / 75;\n 1. Вёрстка соответствует макету. Ширина экрана 768px +48/48;\n 2. Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки +15/15;\n 3. На ширине экрана 768рх и меньше реализовано адаптивное меню +22/22;`)
