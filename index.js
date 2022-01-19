e5090c74b0384081cb2cf283a7c97d76d658ba15
KotenkoNata <kotenkonata8@gmail.com>
2022-01-19 14:43:18 +0200

    feat: changing language

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { i18Obj } from './translate.js';
+
 (() => {
   const refs = {
     openModalBtn: document.querySelector(".header-burger"),
@@ -50,7 +52,6 @@ function preloadSummerImages() {
 
 preloadSummerImages();
 
-// ================
 //Подсветка активной кнопки
 
 const allButtons = document.querySelectorAll('.button');
@@ -65,7 +66,26 @@ function changeClassActive(className) {
 allButtons.forEach(el => {
   el.classList.remove('activeBtn');
   el.addEventListener('click', changeClassActive("activeBtn"))
+
 })
+
+//Подготовка файлов с переводом
+
+const languageButtons = document.querySelectorAll('[data-language]');
+
+function getTranslate(event){
+    let language = event.target.dataset.language;
+    const allElements = document.querySelectorAll('[data-i18]');
+    allElements.forEach(element => {
+      element.textContent = i18Obj[language][element.dataset.i18];
+    })
+}
+
+languageButtons.forEach(el => {
+  el.addEventListener('click',getTranslate)
+})
+
+
 
 
 console.log(`Score: 75 / 75;\n 1. Вёрстка соответствует макету. Ширина экрана 768px +48/48;\n 2. Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки +15/15;\n 3. На ширине экрана 768рх и меньше реализовано адаптивное меню +22/22;`)
