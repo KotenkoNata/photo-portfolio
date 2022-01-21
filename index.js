6a6a8248339691a7b12a5edec7acc6fc51ad3638
KotenkoNata <kotenkonata8@gmail.com>
2022-01-21 14:19:50 +0200

    feat: add styles and js for hero button

--- index.js
+++ index.js
@@ -178,6 +178,34 @@ function getLocalStorage() {
 }
 window.addEventListener('load', getLocalStorage)
 
+//
+
+const buttonRipple = document.querySelector('.ripple');
+
+console.log(buttonRipple)
+
+buttonRipple.addEventListener('click', function (e) {
+  const x = e.clientX
+  const y = e.clientY
+
+  const buttonTop = e.target.offsetTop
+  const buttonLeft = e.target.offsetLeft
+
+
+  const xInside = x - buttonLeft
+  const yInside = y - buttonTop
+
+
+  const circle = document.createElement('span')
+  circle.classList.add('circle')
+  circle.style.top = yInside + 'px'
+  circle.style.left = xInside + 'px'
+
+  this.appendChild(circle)
+
+  setTimeout(() => circle.remove(), 500)
+})
+
 
 
 
